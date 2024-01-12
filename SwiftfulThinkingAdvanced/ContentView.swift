1db69cbcc4b7c9e1f1cf8350ac47db1c9ea6256d
Ben.issa <benesal20034@hotmail.com>
2024-01-12 05:08:16 +0200

    Initial Commit

---
+++ SwiftfulThinkingAdvanced/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftfulThinkingAdvanced
+//
+//  Created by Ben.issa on 12/1/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftfulThinkingAdvanced/SwiftfulThinkingAdvancedApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulThinkingAdvancedApp.swift
+//  SwiftfulThinkingAdvanced
+//
+//  Created by Ben.issa on 12/1/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulThinkingAdvancedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
